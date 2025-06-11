6a2884a36d0c81504afa7427d336af85c6969b4c
Marcio Toze <marcio.toze@gmail.com>
2025-06-10 23:48:03 -0300

    fix assets request

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,8 +22,6 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
-import "../css/app.css"
-
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
