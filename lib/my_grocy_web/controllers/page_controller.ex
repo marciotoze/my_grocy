8eed173264e5a551bbf25830e5e3cf6b6172431d
Marcio Toze <marcio.toze@gmail.com>
2025-06-10 23:16:45 -0300

    fix assets request

--- lib/my_grocy_web/controllers/page_controller.ex
+++ lib/my_grocy_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule MyGrocyWeb.PageController do
   use MyGrocyWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home)
   end
 end
