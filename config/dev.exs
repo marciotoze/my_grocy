31db58e27e1a6a184062cf39fc6fba0758672ac6
Marcio Toze <marcio.toze@gmail.com>
2025-07-14 19:02:22 -0300

    minor fixes

--- config/dev.exs
+++ config/dev.exs
@@ -84,3 +84,5 @@ config :phoenix_live_view,
   debug_heex_annotations: true,
   # Enable helpful, but potentially expensive runtime checks
   enable_expensive_runtime_checks: true
+
+import_config "dev.secret.exs"
---
+++ config/dev.secret.sample.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :my_grocy, :google_api_key, "<YOUR_GOOGLE_API_KEY>"
+config :my_grocy, :google_cse_id, "<YOUR_GOOGLE_CSE_ID>"
+config :my_grocy, :openai_api_key, "<YOUR_OPENAI_API_KEY>"
